126f127517037c21eeee726fae1164606a686939
marielos2 <marieloslpezarana92@gmail.com>
2025-03-20 23:53:05 -0600

    Primer commit

---
+++ main.js
@@ -0,0 +1,21 @@
+//ejercicio 1 Imprime los números del 1 al 10
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+
+
+///ejrcicio 2 imprimir los numero pares del 1 al 30
+
+for (let i = 0; i <= 20; i++){
+    if (i % 2 === 0) {
+        console.log(i);
+     }
+ }
+
+ /// ejercicio 3 crea un triangulo de asteriscos 
+
+ let n = 7;
+for (let i = 1; i <= n; i++) {
+    console.log('*'.repeat(i));
+}
